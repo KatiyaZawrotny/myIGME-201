85b1ad95e19598e909e57602751362a89556be83
kgz5742@rit.edu <kgz5742@rit.edu>
2023-09-01 15:19:39 -0400

    Prints "hello world"

---
+++ Zawrotny_HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿            using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zawrotny_HelloWorld
+{
+    static internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //prints a message to the console
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
