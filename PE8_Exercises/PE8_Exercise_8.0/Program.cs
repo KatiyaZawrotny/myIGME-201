2ba60c06e7f7da15aa3b31d1569adbbc0b23fe86
kgz5742@rit.edu <kgz5742@rit.edu>
2023-09-24 16:09:47 -0400

    initial commit

---
+++ PE8_Exercises/PE8_Exercise_8.0/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_Exercise_8._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //get user input
+            Console.WriteLine("Enter a string:");
+            string input = Console.ReadLine();
+
+            //convert
+            string result = Replace(input, "no", "yes");
+
+            //print     
+            Console.WriteLine(result);
+        }
+
+        static string Replace(string input, string original, string change)
+        {
+            input = input.ToLower();
+            original = original.ToLower();
+
+            return input.Replace(original, change);
+        }
+
+    }
+}
