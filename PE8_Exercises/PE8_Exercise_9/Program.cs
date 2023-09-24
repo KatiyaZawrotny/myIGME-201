adeb96d2aefa0bccc38948846a9f73a919768fde
kgz5742@rit.edu <kgz5742@rit.edu>
2023-09-24 16:23:35 -0400

    initial commit

---
+++ PE8_Exercises/PE8_Exercise_9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_Exercise_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string testString = "This is my string";
+
+            string[] words = testString.Split(' ');
+            string result = "";
+            foreach (string word in words)
+            {
+                result += "\"" + word + "\"" + " ";
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
