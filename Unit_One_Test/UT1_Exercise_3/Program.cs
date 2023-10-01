68de6be63b14ea6ab54edd58db1c9dc47d42e3f3
kgz5742@rit.edu <kgz5742@rit.edu>
2023-10-01 10:46:00 -0400

    Added base implementation of delegate memthod

---
+++ Unit_One_Test/UT1_Exercise_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UT1_Exercise_3
+{
+    internal class Program
+    {
+        //declare delegate
+        delegate double RoundImpersonation(double value, int places);
+
+        //Prints a rounded version of a number using a delegate method.
+        static void Main(string[] args)
+        {
+            //instantiate delegate
+            RoundImpersonation round = Math.Round;
+            //call delegate and assign to result variable
+            double dFinalValue = round(2.83333, 2);
+            //print
+            Console.WriteLine(dFinalValue);
+        }
+    }
+}
