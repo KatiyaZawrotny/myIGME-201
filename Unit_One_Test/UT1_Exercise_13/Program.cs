9e9faa750466ac8b7ce7d0982621c1599add4577
kgz5742@rit.edu <kgz5742@rit.edu>
2023-10-01 12:50:07 -0400

    Changed code to use a struct

--- Unit_One_Test/UT1_Exercise_13/Program.cs
+++ Unit_One_Test/UT1_Exercise_13/Program.cs
@@ -8,20 +8,31 @@ namespace UT1_Exercise_12
 {
     internal class Program
     {
+        struct Employee
+        {
+            public string sName;
+            public double dSalary;
+        }
+
         static void Main(string[] args)
         {
-            string sName;
-            double dSalary = 30000;
+            //Initialize employee info
+            Employee employee = new Employee();
+            employee.dSalary = 30000;
+
             //It should prompt for the user's name
             Console.Write("What is your name? -> ");
-            sName = Console.ReadLine();
+
+            //update employee with new info (name)
+            employee.sName = Console.ReadLine();
+
             //then call the following function:
-            bool bGotRaise = GiveRaise(sName, ref dSalary);
+            bool bGotRaise = GiveRaise(ref employee);
 
             //The main program should congratulate the user if they got a raise, and display their new salary.
             if (bGotRaise)
             {
-                Console.WriteLine($"Congratulations on getting a raise, {sName}! Your new salary is {dSalary}.");
+                Console.WriteLine($"Congratulations on getting a raise, {employee.sName}! Your new salary is {employee.dSalary}.");
             }
             else
             {
@@ -30,12 +41,12 @@ namespace UT1_Exercise_12
 
 
         }
-        static bool GiveRaise(string name, ref double salary)
+        static bool GiveRaise(ref Employee employee)
         {
             //The function should increase the salary by $19,999.99 if name = your name and return true
-            if (name.Equals("Katiya"))
+            if (employee.sName.Equals("Katiya"))
             {
-                salary += 19999.99;
+                employee.dSalary += 19999.99;
                 return true;
             }
             //Otherwise it should return false.
