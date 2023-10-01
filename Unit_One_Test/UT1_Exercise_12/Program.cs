070ae6383fcc44404373447ac64999e6ed1d0573
kgz5742@rit.edu <kgz5742@rit.edu>
2023-10-01 12:40:31 -0400

    stubbed out code

---
+++ Unit_One_Test/UT1_Exercise_12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UT1_Exercise_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sName;
+            double dSalary = 30000;
+            //It should prompt for the user's name
+            //then call the following function:
+            //The function should increase the salary by $19,999.99 if name = your name and return true
+            //Otherwise it should return false.
+            //The main program should congratulate the user if they got a raise, and display their new salary.
+
+        }
+        static bool GiveRaise(string name, ref double salary)
+        {
+            return true;
+        }
+    }
+}
