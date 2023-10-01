66d02e1e53c268f8332fa5069eb47bdb35861751
kgz5742@rit.edu <kgz5742@rit.edu>
2023-10-01 12:44:59 -0400

    Finished program

--- Unit_One_Test/UT1_Exercise_12/Program.cs
+++ Unit_One_Test/UT1_Exercise_12/Program.cs
@@ -13,15 +13,33 @@ namespace UT1_Exercise_12
             string sName;
             double dSalary = 30000;
             //It should prompt for the user's name
+            Console.Write("What is your name? -> ");
+            sName = Console.ReadLine();
             //then call the following function:
-            //The function should increase the salary by $19,999.99 if name = your name and return true
-            //Otherwise it should return false.
+            bool bGotRaise = GiveRaise(sName, ref dSalary);
+
             //The main program should congratulate the user if they got a raise, and display their new salary.
+            if (bGotRaise)
+            {
+                Console.WriteLine($"Congratulations on getting a raise, {sName}! Your new salary is {dSalary}.");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, but you did not get a raise.");
+            }
+            
 
         }
         static bool GiveRaise(string name, ref double salary)
         {
-            return true;
+            //The function should increase the salary by $19,999.99 if name = your name and return true
+            if (name.Equals("Katiya"))
+            {
+                salary += 19999.99;
+                return true;
+            }
+            //Otherwise it should return false.
+            return false;
         }
     }
 }
