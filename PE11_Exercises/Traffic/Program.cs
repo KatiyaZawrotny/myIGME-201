b3efdb2727360438820026035f804b521a793645
kgz5742@rit.edu <kgz5742@rit.edu>
2023-10-10 21:17:47 -0400

    Added up to main

--- PE11_Exercises/Traffic/Program.cs
+++ PE11_Exercises/Traffic/Program.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vehicles;
 
+//Program simulates traffic using the vehicle classes we created
 namespace Traffic
 {
+    //Program simulates traffic using the vehicle classes we created
     internal class Program
     {
+        public void AddPassenger(IPassengerCarrier passengerCarrier)
+        {
+            passengerCarrier.LoadPassenger();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
         static void Main(string[] args)
         {
+            Pickup pickup = new Pickup();
+            FreightTrain freightTrain = new FreightTrain();
+
+
         }
     }
 }
