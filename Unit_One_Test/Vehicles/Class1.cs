ee3a971f06f60390463e5ca814043b823812d849
kgz5742@rit.edu <kgz5742@rit.edu>
2023-10-10 20:49:53 -0400

    Added up to Pickup

--- Unit_One_Test/Vehicles/Class1.cs
+++ Unit_One_Test/Vehicles/Class1.cs
@@ -13,5 +13,46 @@ namespace Vehicles
         public void LoadPassenger() { }
     }
 
+    //Interface for a general Passenger Carrier. Includes LoadPassenger method
+    public interface IPassengerCarrier
+    {
+        //declaration for method that will load a passenger
+        void LoadPassenger();
+    }
+
+    //Interface for a general Heavy Load Carrier
+    public interface IHeavyLoadCarrier
+    {
+
+    }
+
+    //abstract class for the definition of a car
+    public abstract class Car : Vehicle
+    {
+
+    }
+    //abstract class for the definition of a Train
+    public abstract class Train : Vehicle
+    {
+
+    }
+
+    // Class declaration for a compact that uses Car and IPassengerCarrier
+    public class Compact : Car , IPassengerCarrier
+    {
+        
+    }
+
+    // Class declaration for an SUV that uses Car and IPassengerCarrier
+    public class SUV : Car, IPassengerCarrier
+    {
+
+    }
+
+    // Class declaration for a compact that uses Car and IHeavyLoadCarrier
+    public class Pickup : Car, IHeavyLoadCarrier
+    {
+
+    }
 
 }
