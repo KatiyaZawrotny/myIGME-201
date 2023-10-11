d865b76c85ca3081a9eb42b03f2c1b7457e7a15f
kgz5742@rit.edu <kgz5742@rit.edu>
2023-10-10 20:38:32 -0400

    Implemented Vehicles

---
+++ Unit_One_Test/Vehicles/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Collection of classes for the operation of vehicles
+namespace Vehicles
+{
+    //Parent abstract vehicle class with methods declared
+    public abstract class Vehicle
+    {
+        public void LoadPassenger() { }
+    }
+
+
+}
