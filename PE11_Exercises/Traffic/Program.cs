8ce8a08a3716500ee47fdf8f4467b68f50b4277b
kgz5742@rit.edu <kgz5742@rit.edu>
2023-10-10 21:23:14 -0400

    Finished implementing traffic code

--- PE11_Exercises/Traffic/Program.cs
+++ PE11_Exercises/Traffic/Program.cs
@@ -11,20 +11,35 @@ namespace Traffic
     //Program simulates traffic using the vehicle classes we created
     internal class Program
     {
-        public void AddPassenger(IPassengerCarrier passengerCarrier)
+        //Adds a passenger to an IPassengerCarrier by calling the inherited LoadPassenger method from IPassengerCarrier interface.
+        public static void AddPassenger(IPassengerCarrier passengerCarrier)
         {
             passengerCarrier.LoadPassenger();
+            Console.WriteLine("Passenger loaded!");
         }
 
+        //Returns a string version of whhatever the method is enacted upon
         public override string ToString()
         {
             return base.ToString();
         }
+
+        //Main creates testing variables and then calls their ToString() and adds a passenger if appropriate
         static void Main(string[] args)
         {
+            //Initialize two vehicles for testing
             Pickup pickup = new Pickup();
             FreightTrain freightTrain = new FreightTrain();
 
+            //Testing toString
+            Console.WriteLine(pickup.ToString());
+            //Add passenger successfully
+            AddPassenger(pickup);
+
+            Console.WriteLine(freightTrain.ToString());
+            //AddPassenger(freightTrain);
+
+            //Adding a non-IPassengerCarrier class causes the program to crash.
 
         }
     }
