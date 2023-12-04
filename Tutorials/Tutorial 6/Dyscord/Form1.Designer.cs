215080916e52b0364d22dd209c55b59f92ee8952
kgz5742@rit.edu <kgz5742@rit.edu>
2023-12-03 22:16:21 -0500

    Push

--- Tutorials/Tutorial 6/Dyscord/Form1.Designer.cs
+++ Tutorials/Tutorial 6/Dyscord/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Dyscord
 {
-    partial class Form1
+    partial class SettingsForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.startButton = new System.Windows.Forms.Button();
+            this.portTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(44, 96);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(138, 74);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start Listener";
+            this.startButton.UseVisualStyleBackColor = true;
+            // 
+            // portTextBox
+            // 
+            this.portTextBox.Location = new System.Drawing.Point(113, 44);
+            this.portTextBox.Name = "portTextBox";
+            this.portTextBox.Size = new System.Drawing.Size(100, 22);
+            this.portTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Listener Port:";
+            // 
+            // Setting
+            // 
+            this.AcceptButton = this.startButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(261, 233);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.portTextBox);
+            this.Controls.Add(this.startButton);
+            this.Name = "Setting";
+            this.Text = "Settings";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TextBox portTextBox;
+        private System.Windows.Forms.Label label1;
     }
 }
 
---
+++ Tutorials/Tutorial 6/Dyscord/Form2.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Dyscord
+{
+    public delegate void UpdateConversationDelegate(string text);
+    public partial class DyscordForm : Form
+    {
+        string targetUser;
+        string targetIp;
+        int targetPort;
+        string myIp;
+        int myPort = 2222;
+        Thread thread;
+        Socket listener;
+        
+        public DyscordForm()
+        {
+            InitializeComponent();
+
+            this.Show();
+            SettingsForm settingsForm = new SettingsForm(this, myPort);
+            settingsForm.ShowDialog();
+            this.myPort = settingsForm.myPort;
+
+            ThreadStart threadStart = new ThreadStart(Listen);
+            thread = new Thread(threadStart);
+            thread.Start();
+
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            foreach(IPAddress iPAddress in ipHost.AddressList)
+            {
+                if (iPAddress.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    this.myIp = ipHost.ToString();
+                    break;
+                }
+            }
+
+            this.loginButton.Click += new EventHandler(LoginButton__Click);
+            this.usersButton.Click += new EventHandler(UsersButton__Click);
+            this.sendButton.Click += new EventHandler(SendButton__Click);
+            this.exitButton.Click += new EventHandler(ExitButton__Click);
+            this.webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(WebBrowser1__DocumentCompleted);
+        }
+
+        private void LoginButton__Click(object sender, EventArgs e)
+        {
+            if (userTextBox.TextLength > 0)
+            {
+                webBrowser1.Navigate();
+            }
+        }
+
+        public void UpdateConversation(string text)
+        {
+            this.convRichTextBox.Text += text + "\n";
+        }
+        public void Listen()
+        {
+            UpdateConversationDelegate updateConversationDelegate;
+            updateConversationDelegate = new UpdateConversationDelegate(UpdateConversation);
+            IPEndPoint serverEndpoint = new IPEndPoint(IPAddress.Any, this.myPort);
+
+            this.listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(serverEndpoint);
+
+            listener.Listen(300);
+
+            while(true)
+            {
+                Socket client = listener.Accept();
+                Stream netStream = new NetworkStream(client);
+                StreamReader reader = new StreamReader(netStream);
+                string result = reader.ReadToEnd();
+                Invoke(updateConversationDelegate, result);
+                reader.Close();
+                netStream.Close();
+                client.Close();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
