2718f7e900b31859627a831efb1812f2401f2260
kgz5742@rit.edu <kgz5742@rit.edu>
2023-12-18 14:34:34 -0500

    Finished stack

---
+++ Final_Exam/Final_Exam_Q1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Stack 
+{
+    //MyStack creates a replica of the Stack<int> data structure
+    class MyStack
+    {
+        //List to represent the items in our stack
+        private List<int> items = new List<int>();
+
+        //Adds a new item to the stack at the top
+        public void Push(int n)
+        {
+            items.Add(n);
+        }
+
+        //Takes the top item out of the stack and returns it
+        public int Pop()
+        {
+            if (items.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty.");
+            }
+
+            int prevIndex = items.Count - 1;
+            int popped = items[prevIndex];
+            items.RemoveAt(prevIndex);
+            return popped;
+        }
+
+        //returns the item at the top of the stack without modifying it
+        public int Peek()
+        {
+            if (items.Count == 0)
+            {
+                throw new Exception("The stack is empty.");
+            }
+
+            return items[items.Count - 1];
+        }
+    }
+    
+}
