0aa8e3dfa02cdc3e2dc0f18dc4c080d13c5678b7
kgz5742@rit.edu <kgz5742@rit.edu>
2023-12-18 14:47:35 -0500

    Finished queue

---
+++ Final_Exam/Final_Exam_Q2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace Final_Exam_Q2
+{
+    //MyQueue replicates the Queue<int> data structure
+    class MyQueue
+    {
+        //List to represent items in our queue
+        private List<int> items = new List<int>();
+
+        //Adds an item to the back of the queue
+        public void Enqueue(int n)
+        {
+            items.Add(n);
+        }
+        
+        //Removes the item at the front of the queue and returns it
+        public int Dequeue()
+        {
+            if (items.Count == 0)
+            {
+                throw new Exception("The queue is empty.");
+            }
+
+            int dequeuedItem = items[0];
+            items.RemoveAt(0);
+            return dequeuedItem;
+        }
+
+        //Returns the item at the front of the queue
+        public int Peek()
+        {
+            if (items.Count == 0)
+            {
+                throw new Exception("The queue is empty.");
+            }
+
+            return items[0];
+        }
+    }
+}
